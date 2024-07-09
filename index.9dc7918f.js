ae321f539bdf452172e65bd67c82f757b11354fc
Seva Podolskiy <s_podolskiy@live.com>
2024-07-09 11:27:26 -0400

    make build

---
+++ index.9dc7918f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#thumbs"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){e.preventDefault(),t.src=e.target.closest(".list-item__link").href});
+//# sourceMappingURL=index.9dc7918f.js.map
